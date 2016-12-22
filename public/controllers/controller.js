092a8fc12b44124aee23bcb08a4f39b1b613aeb9
root <volverine.felix@gmail.com>
2016-12-22 01:54:55 -0200

    inserindo e refrescando automaticamente

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,23 +5,50 @@ angular.module("app", [])
 // .service('myService', function () {})
 .controller('AppCtrl', function($scope, $http) 
 {
-	// rota que aciona função callback no server.js (node) linkado pela url e recebe um resultado
-	// $http.get('/listacontatos').success(function(resp){
-	// 	$scope.listaContatos = resp;
-	// })
+	// função que refresca automaticamente a pagina na respsta do server
+	var refresh = function()
+	{
+		// rota que aciona função callback no server.js (node) linkado pela url e recebe um resultado
+		$http
+		({
+		  method: 'GET',
+		  url: '/listacontatos'
+		})
+		.then(function successCallback(resp) 
+		{
+			console.log(resp.data);
+		    $scope.listaContatos = resp.data;
+		    // $scope.contato = "";
+		}, 
+		function errorCallback(resp) 
+		{
+		    alert('azedo o pé do frango');
+		});
+	};
 
-	$http
-	({
-	  method: 'GET',
-	  url: '/listacontatos'
-	})
-	.then(function successCallback(resp) 
+	// chamando a função refresh que traz a lista de contatos atualizada
+	refresh();
+
+	$scope.addContato = function()
 	{
-		console.log(resp.data);
-	    $scope.listaContatos = resp.data;
-	}, 
-	function errorCallback(resp) 
-	{
-	    alert('azedo o pé do frango');
-	});
+		console.log($scope.contato);
+
+		$http
+		({
+		  method: 'POST',
+		  url: '/listacontatos',
+		  data: $scope.contato
+		})
+		.then(function successCallback(resp) 
+		{
+			console.log(resp);
+			refresh();
+			// limpando os dados dos campos de inserção no sucesso da resposta
+			$scope.contato = "";
+		}, 
+		function errorCallback(resp) 
+		{
+		    alert('deu ruim no insert!!');
+		});
+	};
 });
--- server.js
+++ server.js
@@ -4,44 +4,42 @@ var express = require('express');
 // agora app pode acessar os metodos da classe express
 var app = express();
 
+// estanciando mongojs
+var mongojs = require('mongojs');
+
+// pegando os objetos do banco
+var db = mongojs('db_lista_contatos', ['db_lista_contatos']);
+
+// estancia do body parser
+var bodyParser = require('body-parser');
+
 // para entregar os arquivos estaticos
 app.use(express.static(__dirname + "/public"));
+
+// utilizando o body-parser
+app.use(bodyParser());
 
 // funções com callback acionada pela chamada http na controller (GET)
 app.get('/listacontatos', function(req, res)
 {
 	console.log('recebi uma requisição da lista de contatos!');
 
-	// criando a lista de contatos
-	var contato1 = 
-	{
-		nome: "jonas",
-		email: "jonas@jonas.com",
-		fone: "11 911112233"
-	};
+	db.db_lista_contatos.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
 
-	var contato2 = 
-	{
-		nome: "joane",
-		email: "joane@joane.com",
-		fone: "11 933338844"
-	};
-
-	var contato3 = 
-	{
-		nome: "joshua",
-		email: "joshua@joshua.com",
-		fone: "11 977778844"
-	};
-
-	// compactando a lista de contatos
-	var listaContatos = [contato1, contato2, contato3];
-
-	// responde para a controller que esta fazendo a chamada get http linkada pela url um jason
-	res.json(listaContatos);
+// recebendo post da controller e inserindo na base db_lista_contatos
+app.post('/listacontatos', function(req, res)
+{
+	console.log(req.body);
+	db.db_lista_contatos.insert(req.body, function(err, docs){
+		res.json(docs);
+	});
 });
 
 
 // porta do servidor que vai subir a aplicação
 app.listen(3000)
-console.log('mensagem que só aparece no servidor');
+console.log('estou no servidor porta 3000!!');
